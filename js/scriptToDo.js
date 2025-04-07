1d12f9cabe490d521197fa9d63e9bb1a73b50610
DustinLWagner <DustinWagner@gmail.com>
2025-04-07 12:21:22 -0500

    tiny progress towards local storage

--- js/scriptToDo.js
+++ js/scriptToDo.js
@@ -15,22 +15,6 @@ document.getElementById("clear").addEventListener("click", function () {
     });
 
 });
-
-
-//report to console Checked and Unchecked
-document.getElementById("itemsList").addEventListener("change", function (event) {
-    if (event.target && event.target.name === "box") {
-        if (event.target.checked) {
-            console.log("checked box");
-        } else {
-            console.log("unchecked box");
-        }
-    };
-});
-
-
-
-
 //create and add to do item *ADD PROMPT FOR ITEM NAME/DESCRIPTION
 document.querySelector("button[id=new]").addEventListener("click", function () {
     //prompt user for new task
@@ -45,7 +29,49 @@ document.querySelector("button[id=new]").addEventListener("click", function () {
     let itemsList = document.getElementById("itemsList");
     itemsList.appendChild(listItem);
 });
+//----------------------set up local storage------------------//
+
+//store li items in tasks
+let tasks = [];
+let itemsList = document.querySelectorAll("#itemsList li");
+itemsList.forEach(item => {
+    tasks.push(item.innerText);
+});
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+//check local storage for tasks
+let storedTasks = JSON.parse(localStorage.getItem("tasks"));
+if (storedTasks === null) {
+    console.log("List Empty")
+} else {
+    console.log("has items")
+    //if task exists iterate through and create list items for each
+};
 
 
 
+//When the user adds a task via the prompt, save that task in localStorage
+//Append the new task as a list item on the page.
+
+//user checks box and clicks "clear" remove the corresponding task from both the page and localStorage.
+
+//save task Convert your list of tasks to a string format (e.g., JSON.stringify) and store it in localStorage.
+
+//retrieve task localStorage.getItem() to retrieve the list
+
+
+
+
+
+
+//report to console Checked and Unchecked
+document.getElementById("itemsList").addEventListener("change", function (event) {
+    if (event.target && event.target.name === "box") {
+        if (event.target.checked) {
+            console.log("checked box");
+        } else {
+            console.log("unchecked box");
+        }
+    };
+});
 
