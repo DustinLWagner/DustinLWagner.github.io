b8386e82804fffb90c8fe40dfe6f5b998b231c40
DustinLWagner <DustinWagner@gmail.com>
2025-04-06 23:13:53 -0500

    created styles pages for each page splitting up the styles2.css for readability and ease of change. Upgraded new button to create actual li item with checkbox, added selector & listeners to determine checkboxes check or not.

--- js/scriptToDo.js
+++ js/scriptToDo.js
@@ -1,14 +1,33 @@
 //clear list item with ID=box1
 document.getElementById("clear").addEventListener("click", function () {
     console.log("You clicked Clear");
-    document.getElementById("box1").innerText = ("")
+    document.getElementById("box").innerText = ("");
 });
 
-document.getElementById("new").addEventListener("click", function () {
+//get all check boxes and report to console when checked and unchecked
+let checkboxes = document.querySelectorAll("input[name=box]");
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            console.log("checkbox is checked");
+        } else {
+            console.log("unchecked box")
+        }
+    });
+});
+
+
+
+
+//create and add to do item *ADD PROMPT FOR ITEM NAME/DESCRIPTION
+document.querySelector("button[id=new]").addEventListener("click", function () {
     console.log("You clicked New");
     //create new list item
     let listItem = document.createElement("li");
-    listItem.textContent = "New Item";
+    //listItem.textContent = "New Item";
+    listItem.innerHTML = '<input name="box" type="checkbox"> Item </input>';
+
     //add new list item to bottom of list
     let itemsList = document.getElementById("itemsList");
     itemsList.appendChild(listItem);
