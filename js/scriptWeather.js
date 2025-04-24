14eb3b364fbcc7c206fdc4082d8c7caf52eaf444
DustinLWagner <DustinWagner@gmail.com>
2025-04-24 14:51:01 -0500

    Weather App, Added Submit button, got the api request working with no errors

---
+++ js/scriptWeather.js
@@ -0,0 +1,26 @@
+/* openweathermap API key = d7abb5e8c603fa467ae7ba6725571b70
+const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode}&appid=${apiKey}`;
+*/
+
+
+const apiKey = "d7abb5e8c603fa467ae7ba6725571b70"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById("zip");
+    const button = document.getElementById("zipSubmit");
+
+    button.addEventListener('click', () => {
+        console.log('clicked submit');
+        const zip = input.value;
+        const countryCode = 'us';
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode}&appid=${apiKey}`;
+        console.log('Zip code entered:', zip);
+        fetch(url);
+
+    });
+});
+
+
+
+
