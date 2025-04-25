f4012da2093bf99a2c5ccd8f6d45572933eb66fc
DustinLWagner <DustinWagner@gmail.com>
2025-04-25 12:34:13 -0500

    weatherapp, added feels like temp, wind speed and direction, function to convert wind angle to direction, reworded weather msg

--- js/scriptWeather.js
+++ js/scriptWeather.js
@@ -1,10 +1,3 @@
-/* openweathermap API key = d7abb5e8c603fa467ae7ba6725571b70
-const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode}&appid=${apiKey}`;
-
-
-
-*/
-
 
 const apiKey = "d7abb5e8c603fa467ae7ba6725571b70"
 
@@ -20,17 +13,28 @@ document.addEventListener('DOMContentLoaded', () => {
         let zip = input.value;
         let countryCode = 'us';
 
-
         const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode}&appid=${apiKey}&units=imperial`;
         console.log('Zip code entered:', zip);
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                let temp = Math.trunc(data.main.temp)
-                let city = data.name
-                let description = data.weather[0].description
-                msg.innerText = "Weather in " + city + " is " + description + " and it is " + temp + "°F";
+
+                function getWindDir(angle) {
+                    const directions = ['↓ N', '↙ NE', '← E', '↖ SE', '↑ S', '↗ SW', '→ W', '↘ NW'];
+                    return directions[Math.round(angle / 45) % 8];
+                };
+
+                let temp = Math.trunc(data.main.temp);
+                let feelsLike = Math.trunc(data.main.feels_like);
+
+                let city = data.name;
+                let description = data.weather[0].description;
+                let windSpeed = Math.trunc(data.wind.speed);
+                let windDir = getWindDir(data.wind.deg);
+
+                msg.innerText = " It is " + temp + "°F" + " in " + city + " right now and feels like " + feelsLike + "°F" + " with " + description + " and " + windDir + " " + windSpeed + " Mph " + " winds. ";
+
                 weatherPic.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
                 input.value = '';
             });
@@ -38,4 +42,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 });
+/*
 
+wind: 
+deg: 140
+speed: 3.44
+
+"The weather in " + city + " right now is " + temp + "°F" + " and feels like " feelsLike + "°F" + " and it is " + description + " outside.";
+
+
+msg.innerText = "Weather in " + city + " right now is " + description + " and it is " + temp + "°F " + " and feels like " + feelsLike + "°F" + " outside.";
+*/
