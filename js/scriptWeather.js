fde6e5fe127fecb33c3a176648d7fcfb1af0278f
DustinLWagner <DustinWagner@gmail.com>
2025-04-25 15:00:33 -0500

    added zipcode validation, fixed cardinal directions, reworded msg,

--- js/scriptWeather.js
+++ js/scriptWeather.js
@@ -1,45 +1,54 @@
 
 const apiKey = "d7abb5e8c603fa467ae7ba6725571b70"
 
-
 document.addEventListener('DOMContentLoaded', () => {
+
     let input = document.getElementById("zip");
     const button = document.getElementById("zipSubmit");
     const weatherBox = document.getElementById("weatherBox");
     button.addEventListener('click', () => {
-        console.log('clicked submit');
         weatherBox.classList.add('openWeatherBox');
         let msg = document.querySelector('.msg');
         let weatherPic = document.getElementById('weatherPic');
-        let zip = input.value;
+        let zip = input.value.trim();
         let countryCode = 'us';
         let temps = document.getElementById('temps');
-
         const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode}&appid=${apiKey}&units=imperial`;
-        console.log('Zip code entered:', zip);
+
+        //check for valid zipcode
+        function isValidZip(zip) {
+            return /^\d{5}(-\d{4})?$/.test(zip);
+        };
+
+        if (!isValidZip(zip)) {
+            msg.innerText = "Please enter a valid ZIP code (e.g., 12345 or 12345-6789).";
+            input.value = "";
+            return;
+        }
+
+        //call api
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-
+                // convert wind angle to direction    
                 function getWindDir(angle) {
-                    const directions = ['↓ N', '↙ NE', '← E', '↖ SE', '↑ S', '↗ SW', '→ W', '↘ NW'];
+                    const directions = ['↓ North', '↙ NE', '← East', '↖ SE', '↑ South', '↗ SW', '→ West', '↘ NW'];
                     return directions[Math.round(angle / 45) % 8];
                 };
-
+                // get data from api response    
+                let city = document.getElementById('city');
                 let temp = Math.trunc(data.main.temp);
                 let feelsLike = Math.trunc(data.main.feels_like);
-
                 let location = data.name;
                 let description = data.weather[0].description;
                 let windSpeed = Math.trunc(data.wind.speed);
                 let windDir = getWindDir(data.wind.deg);
-
+                // message contents    
                 city.innerText = location;
                 temps.innerText = temp + "°F / " + feelsLike + "°F";
 
-
-                msg.innerText = description + " and " + windDir + " " + windSpeed + " Mph " + " winds. ";
+                msg.innerText = description + " with " + windSpeed + " Mph " + " winds out of the " + windDir;
 
                 weatherPic.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
                 input.value = '';
